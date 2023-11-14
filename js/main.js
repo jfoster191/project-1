ca45c660f78425a6269ab1642bb2dc66eca1ca24
jfoster <jfoster191@hotmail.com>
2023-11-14 15:13:21 -0500

    adds functionality for check answer button; adds medium board; adds hideBoard function for start; adds userChoice function for selecting difficulty

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@ const solutionBoard4x4 = [
 ];
 
 //State Variables
+let puzzleBoard
 let puzzleBoard4x4;
+let puzzleBoard6x6;
+let choice;
 let solvedCorrectly;
 let currentInput;
 let currentCell;
 let lastCell;
 let initial;
+let checkArr;
 
 //Page Elements to be Accessed Frequently
-const gridEl = [...document.querySelectorAll('#board-4-4 > div')];
+let gridEl;
 const submitBtn = document.getElementById('submit');
 const checkBtn = document.getElementById('check');
 const resetBtn = document.getElementById('reset');
@@ -24,11 +28,16 @@ const messageEl = document.querySelector('h1');
 
 //Event Listeners
 submitBtn.addEventListener('click', submitClick);
-document.querySelector('section').addEventListener('click', gridClick);
-resetBtn.addEventListener('click', resetClick)
+document.getElementById('board-4-4').addEventListener('click', gridClick);
+document.getElementById('board-6-6').addEventListener('click', gridClick);
+resetBtn.addEventListener('click', resetClick);
+checkBtn.addEventListener('click', checkAnsClick);
+document.getElementById('board-4').addEventListener('click', userChoice)
+document.getElementById('board-6').addEventListener('click', userChoice)
+document.getElementById('board-9').addEventListener('click', userChoice)
 
 //Main
-init ();
+hideGrids();
 
 //Functions
 
@@ -39,8 +48,19 @@ function init () {
     null, null, 3, 2,
     null, null, null, null
  ];
+ puzzleBoard6x6 = [
+    5, null, null, null, 6, null,
+    null, 4, null, 1, null, 3,
+    1, null, 3, null, 4, null,
+    null, null, 2, null, 1, null,
+    null, 1, 4, 6, null, 5,
+    null, 6, null, 4, null, null
+ ]
+ if (choice === 4){ puzzleBoard = puzzleBoard4x4;}
+ else if (choice === 6){ puzzleBoard = puzzleBoard6x6}
  solvedCorrectly = null;
  lastCell = null;
+ checkArr = [];
  initial = 1;
  render ();
  initial = 0;
@@ -52,12 +72,12 @@ function render () {
 }
 
 function renderBoard () {
-    puzzleBoard4x4.forEach(function(cell, idx) {
+    puzzleBoard.forEach(function(cell, idx) {
         const cellPos = idx;
         const cellEl = document.getElementById(cellPos);
-        if (puzzleBoard4x4[idx] === null){return}
+        if (puzzleBoard[idx] === null){return}
         else{
-            cellEl.innerText = puzzleBoard4x4[idx];
+            cellEl.innerText = puzzleBoard[idx];
             if (initial === 1){
                 cellEl.style.fontWeight = 'bold';
                 cellEl.style.backgroundColor = '#D4D4D4';
@@ -67,9 +87,20 @@ function renderBoard () {
 }
 
 function renderMessage (){
+    if (choice === null){
+        messageEl.innerText = 'Please Make Your Difficulty Selection'
+        return
+    }
     if (initial === 1 || currentInput){ messageEl.innerText = ''}
     else if(!currentInput){
     messageEl.innerText = 'Please Enter A Valid Number';
+    }
+    if (solvedCorrectly === 1){
+        messageEl.innerText = 'Congrats You Sovled the Puzzle Correctly!'
+    }
+    else if (solvedCorrectly === 0){
+        messageEl.innerText = 'Take a Look Again, Somethings Not Quite Right'
+        solvedCorrectly = null;
     }
 }
 
@@ -94,9 +125,52 @@ function gridClick (evt) {
         currentCell.style.backgroundColor = '#808080';
 }
 
+function checkAnsClick () {
+    gridEl.forEach(function(elem, idx){
+        checkArr[idx] = elem.innerText;
+    });
+    if (checkArr.toString() === solutionBoard4x4.toString()){ solvedCorrectly = 1;} 
+    else {solvedCorrectly = 0;}
+    console.log(checkArr);
+    console.log(solvedCorrectly);
+    render();
+}
+
 function resetClick (){
     gridEl.forEach(function(element){
         element.innerText = '';
     })
     init ();
+}
+
+function userChoice (evt){
+    if (evt.target.id === 'board-4'){
+        gridEl = [...document.querySelectorAll('#board-4-4 > div')];
+        gridEl.forEach(function(cell){
+            cell.removeAttribute('hidden')
+            choice = 4;
+        })
+    }
+    else if (evt.target.id === 'board-6'){
+        gridEl = [...document.querySelectorAll('#board-6-6 > div')]
+        gridEl.forEach(function(cell){
+            cell.removeAttribute('hidden')
+            choice = 6;
+        })
+    }
+    init();
+}
+
+function hideGrids (){
+    gridEl = [...document.querySelectorAll('#board-4-4 > div')];
+    gridEl.forEach(function(cell){
+        cell.setAttribute('hidden', 'hidden')
+    })
+
+    gridEl = [...document.querySelectorAll('#board-6-6 > div')];
+    gridEl.forEach(function(cell){
+        cell.setAttribute('hidden', 'hidden')
+    })
+    choice = null;
+    renderMessage();
 }
